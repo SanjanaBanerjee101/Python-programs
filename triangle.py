92cecce3af2c14076a90f9c3b352f9f5012fa47d
FriaElegante <mail2sanjanaban@gmail.com>
2025-07-18 00:03:38 +0530

    Add selected triangle Python file(s) from PyCharm

---
+++ triangle.py
@@ -0,0 +1,11 @@
+import math
+r = int(input("Enter the radius of the circle: "))
+
+print("Circumference = ", 2*math.pi*r)
+print("Area = ", math.pi*pow(r,2))
+
+print("Hypotenuse of a triangle")
+a = int(input("Enter the first side of the triangle: "))
+b = int(input("Enter the second side of the triangle: "))
+
+print("Hypotenuse of a triangle is ", math.sqrt(pow(a,2)+pow(b,2)))
